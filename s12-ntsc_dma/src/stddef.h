6fc4ba59af6e31ca478f2123393467ad501ee16d
Taisuke Fukuno <fukuno@TPC0165.local>
2018-01-08 14:21:53 +0900

    rename stm32f1

---
+++ s12-ntsc_dma/src/stddef.h
@@ -0,0 +1,18 @@
+#ifndef STDDEF_H_
+#define STDDEF_H_
+
+#define NULL 0
+
+#define STRING2(s) STRING(s)
+#define STRING(str) #str
+
+typedef signed char int8;
+typedef signed short int16;
+typedef signed int int32;
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned int uint;
+
+
+#endif
